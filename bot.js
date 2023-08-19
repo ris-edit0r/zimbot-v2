60023e33c14547a06c3624f1a6ebd29f9cce0da0
Rishu <118464946+ris-edit0r@users.noreply.github.com>
2023-08-19 17:51:38 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,15 +33,15 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.owner = ['+917389281978',''] //owner number +917389281978
+global.pemilik = ['+917389281978'] //owner number +917389281978
+global.premium = ['+917389281978'] //premium number ++917389281978
+global.pengguna = 'LEGEND' //username
+global.footer = 'RSF BOT INC'
+global.botnma = 'RSF BOT INC' //bot name
+global.ownernma = 'MR LEGEND' //owner name
+global.packname = 'LEGEND' //sticker package name
+global.author = 'By LEGEND Memes' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
